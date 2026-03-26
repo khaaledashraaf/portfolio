8dca4907a8e566ef07b2531ba37e27f6683dc556
khaaledashraaf <250846853+khaaledashraaf@users.noreply.github.com>
2026-03-26 14:14:12 +0200

    Update clarity-provider.tsx

--- src/components/clarity-provider.tsx
+++ src/components/clarity-provider.tsx
@@ -5,6 +5,7 @@ import Clarity from "@microsoft/clarity";
 
 export function ClarityProvider() {
   useEffect(() => {
+    if (localStorage.getItem("dev") === "true") return;
     Clarity.init(process.env.NEXT_PUBLIC_CLARITY_ID!);
   }, []);
 
